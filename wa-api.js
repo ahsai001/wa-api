bffa0bbd607d4a64af5c48807186d61f7fe05c9f
ahmad s <ahsai001@gmail.com>
2024-09-20 10:03:52 +0700

    handle onmessage for converstaional chat bot

--- wa-api.js
+++ wa-api.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const venom = require('venom-bot');
-const yargs = require('yargs');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const venom = require("venom-bot");
+const yargs = require("yargs");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const argv = yargs.argv;
 const port = argv.port;
-const wa_number = "no"+argv.wanumber;
+const wa_number = "no" + argv.wanumber;
 
 // Middleware untuk parsing JSON
 app.use(express.json());
@@ -17,13 +17,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Set static folder untuk menyajikan file HTML dan aset lainnya
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //destination uploaded file
 // Konfigurasi multer untuk menyimpan file
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const uploadDir = 'uploads/';
+    const uploadDir = "uploads/";
     if (!fs.existsSync(uploadDir)) {
       fs.mkdirSync(uploadDir);
     }
@@ -31,7 +31,7 @@ const storage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
-  }
+  },
 });
 
 const upload = multer({ storage: storage });
@@ -39,84 +39,109 @@ const upload = multer({ storage: storage });
 // Inisialisasi Venom.js
 venom
   .create({
-    session: wa_number //name of session
+    session: wa_number, //name of session
   })
   .then((client) => {
+    client.onMessage((message) => {
+      if (message.body === "Hi" && message.isGroupMsg === false) {
+        client
+          .sendText(message.from, "Welcome Bro")
+          .then((result) => {
+            console.log("Result: ", result); //return object success
+          })
+          .catch((erro) => {
+            console.error("Error when sending: ", erro); //return object error
+          });
+      }
+    });
+
     // Route untuk mengirim pesan
-    app.post('/send-text', async (req, res) => {
+    app.post("/send-text", async (req, res) => {
       const { number, message } = req.body;
 
       if (!number || !message) {
-        return res.status(400).json({ error: 'Nomor dan pesan diperlukan' });
+        return res.status(400).json({ error: "Nomor dan pesan diperlukan" });
       }
 
       try {
         // Kirim pesan menggunakan Venom
-        await client.sendText(number + '@c.us', message);
-        res.json({ success: true, message: 'Pesan berhasil dikirim!' });
+        await client.sendText(number + "@c.us", message);
+        res.json({ success: true, message: "Pesan berhasil dikirim!" });
       } catch (error) {
         console.error(error);
-        res.status(500).json({ success: false, message: 'Gagal mengirim pesan' });
+        res
+          .status(500)
+          .json({ success: false, message: "Gagal mengirim pesan" });
       }
     });
-	
-	
-	app.post('/send-image', upload.single('image'), async (req, res) => {
+
+    app.post("/send-image", upload.single("image"), async (req, res) => {
       const { number, caption } = req.body;
       const imagePath = req.file.path;
 
       if (!number || !req.file) {
-        return res.status(400).json({ error: 'Nomor dan gambar diperlukan' });
+        return res.status(400).json({ error: "Nomor dan gambar diperlukan" });
       }
 
       try {
         // Kirim gambar menggunakan Venom
-        await client.sendImage(number + '@c.us', imagePath, req.file.originalname, caption || '');
-		 res.json({
-			success: true,
-			message: 'Image sent successfully',
-			originalFileName: req.file.originalname,
-			number: number,
-			caption: caption
-		  });
+        await client.sendImage(
+          number + "@c.us",
+          imagePath,
+          req.file.originalname,
+          caption || ""
+        );
+        res.json({
+          success: true,
+          message: "Image sent successfully",
+          originalFileName: req.file.originalname,
+          number: number,
+          caption: caption,
+        });
 
         // Hapus file setelah dikirim
         fs.unlinkSync(imagePath);
       } catch (error) {
-        console.error('Gagal mengirim gambar:', error);
-        res.status(500).json({ success: false, message: 'Gagal mengirim gambar' });
+        console.error("Gagal mengirim gambar:", error);
+        res
+          .status(500)
+          .json({ success: false, message: "Gagal mengirim gambar" });
       }
     });
-	
-	
-	app.post('/send-file', upload.single('document'), async (req, res) => {
+
+    app.post("/send-file", upload.single("document"), async (req, res) => {
       const { number, caption } = req.body;
       const filePath = req.file.path;
 
       if (!number || !req.file) {
-        return res.status(400).json({ error: 'Nomor dan document diperlukan' });
+        return res.status(400).json({ error: "Nomor dan document diperlukan" });
       }
 
       try {
         // Kirim file menggunakan Venom
-        await client.sendFile(number + '@c.us', filePath, req.file.originalname, caption || '');
-		res.json({
-			success: true,
-			message: 'File sent successfully',
-			originalFileName: req.file.originalname,
-			number: number,
-			caption: caption
-		  });
-
+        await client.sendFile(
+          number + "@c.us",
+          filePath,
+          req.file.originalname,
+          caption || ""
+        );
+        res.json({
+          success: true,
+          message: "File sent successfully",
+          originalFileName: req.file.originalname,
+          number: number,
+          caption: caption,
+        });
 
         // Hapus file setelah dikirim
         fs.unlinkSync(filePath);
       } catch (error) {
-        console.error('Gagal mengirim file:', error);
-        res.status(500).json({ success: false, message: 'Gagal mengirim file' });
+        console.error("Gagal mengirim file:", error);
+        res
+          .status(500)
+          .json({ success: false, message: "Gagal mengirim file" });
       }
     });
-
 
     // Start server
     app.listen(port, () => {
@@ -124,5 +149,5 @@ venom
     });
   })
   .catch((error) => {
-    console.log('Gagal memulai Venom:', error);
+    console.log("Gagal memulai Venom:", error);
   });
